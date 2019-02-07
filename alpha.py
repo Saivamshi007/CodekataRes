c4f91dd78f68a054aa75d1ae6f77d85004e74c79
Saivamshi007 <47409641+Saivamshi007@users.noreply.github.com>
2019-02-07 14:46:20 +0530

    Update alpha.py

--- alpha.py
+++ alpha.py
@@ -3,6 +3,6 @@ if ch == '0':
     exit();
 else:
     if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
-    	print("Alphabet.");
+    	print("Alphabet");
     else:
     	print(ch, "is not an alphabet.");
